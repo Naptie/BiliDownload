fba9748fc7e919ea2baab0d027bf8ec071564bbf
Naptie <naptie@qq.com>
2021-04-17 21:30:42 +0800

    Initial Commit

---
+++ src/main/java/me/naptie/bilidownload/Main.java
@@ -0,0 +1,357 @@
+package me.naptie.bilidownload;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import me.naptie.bilidownload.utils.UserAgentManager;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		long beginTime = System.currentTimeMillis();
+		File input = new File("Input.txt");
+		System.out.println("请输入一个AV号或BV号：");
+		Scanner scanner = input.exists() ? new Scanner(input) : new Scanner(System.in);
+		String id = scanner.next();
+		System.out.println("\n请输入Cookie中SESSDATA的值（若无请填“#”）：");
+		String sessData = scanner.next();
+		String infoUrl = "http://api.bilibili.com/x/web-interface/view?" + (id.toLowerCase().startsWith("av") ? "aid=" + id.substring(2) : "bvid=" + id);
+		String cookie = sessData.equals("#") ? "#" : "SESSDATA=" + sessData + "; Path=/; Domain=bilibili.com;";
+		System.out.println("\n正在获取稿件信息······");
+		JSONObject info = readJsonFromUrl(infoUrl, cookie);
+		if (info.getIntValue("code") != 0) {
+			System.out.println(info.getString("message"));
+			return;
+		} else {
+			info = info.getJSONObject("data");
+		}
+		System.out.println("\n标题：" + info.getString("title"));
+		System.out.println("UP主：" + info.getJSONObject("owner").getString("name"));
+		System.out.println("时长：" + getFormattedTime(info.getIntValue("duration"), info.getIntValue("duration") > 3600));
+		System.out.println("播放数：" + String.format("%,d", info.getJSONObject("stat").getIntValue("view")));
+		System.out.println("弹幕数：" + String.format("%,d", info.getJSONObject("stat").getIntValue("danmaku")));
+		System.out.println("获赞数：" + String.format("%,d", info.getJSONObject("stat").getIntValue("like")));
+		System.out.println("投币数：" + String.format("%,d", info.getJSONObject("stat").getIntValue("coin")));
+		System.out.println("收藏数：" + String.format("%,d", info.getJSONObject("stat").getIntValue("favorite")));
+		int videos = info.getIntValue("videos");
+		int cid;
+		String name = info.getString("title");
+		if (videos > 1) {
+			JSONArray pages = info.getJSONArray("pages");
+			System.out.println("\n分P：");
+			for (int i = 0; i < pages.size(); i++) {
+				System.out.println(String.format("%3d", (i + 1)) + ". P" + String.format("%-5d", pages.getJSONObject(i).getIntValue("page")) + "CID：" + pages.getJSONObject(i).getIntValue("cid") + "  时长：" + getFormattedTime(pages.getJSONObject(i).getIntValue("duration"), pages.getJSONObject(i).getIntValue("duration") >= 3600) + "  标题：" + pages.getJSONObject(i).getString("part"));
+			}
+			System.out.println("请选择分P（输入 1~" + pages.size() + " 之间的整数）：");
+			int part = scanner.nextInt();
+			if (part > pages.size()) {
+				System.out.println("输入的数字“" + part + "”太大，已为您选择末尾的分P " + pages.getJSONObject(pages.size() - 1).getString("part"));
+				part = pages.size();
+			}
+			if (part < 1) {
+				System.out.println("输入的数字“" + part + "”太小，已为您选择开头的分P " + pages.getJSONObject(0).getString("part"));
+				part = 1;
+			}
+			cid = pages.getJSONObject(part - 1).getIntValue("cid");
+			name += " [P" + part + "] " + pages.getJSONObject(part - 1).getString("part");
+		} else {
+			cid = info.getIntValue("cid");
+		}
+		System.out.println("\n正在获取清晰度信息······");
+		String videoUrl1 = "https://api.snm0516.aisee.tv/x/tv/ugc/playurl?avid=" + info.getIntValue("aid") + "&mobi_app=android_tv_yst&fnval=16&qn=120&cid=" + cid + "&platform=android&build=103800&fnver=0";
+		String videoUrl2 = "http://api.bilibili.com/x/player/playurl?avid=" + info.getIntValue("aid") + "&cid=" + cid + "&fnval=80&fourk=1";
+		JSONObject video1 = readJsonFromUrl(videoUrl1, cookie);
+		JSONObject video2 = readJsonFromUrl(videoUrl2, cookie).getJSONObject("data");
+		JSONArray qualities1 = video1.getJSONArray("accept_description");
+		JSONArray qualities2 = video2.getJSONArray("accept_description");
+		JSONArray qualities = summary(qualities1, qualities2, video1);
+		System.out.println("\n清晰度：");
+		for (int i = 1; i < qualities.size(); i++) {
+			System.out.println(String.format("%3d", i) + ". " + qualities.getString(i));
+		}
+		System.out.println("请选择清晰度（输入 1~" + (qualities.size() - 1) + " 之间的整数）：");
+		int quality = scanner.nextInt();
+		String videoDownloadUrl;
+		if (qualities.getIntValue(0) == 1) {
+			if (quality > qualities.size() - 1) {
+				System.out.println("输入的数字“" + quality + "”太大，已为您选择最差清晰度 " + qualities.getString(qualities.size() - 1).replaceAll(" +", " "));
+				quality = qualities.size() - 1;
+				videoDownloadUrl = getVideoDownload(video2, qualities2.size() - 1);
+			} else if (quality > qualities1.size())
+				videoDownloadUrl = getVideoDownload(video2, quality - qualities1.size() - 1);
+			else if (quality > 0)
+				videoDownloadUrl = getVideoDownload(video1, quality - 1);
+			else {
+				System.out.println("输入的数字“" + quality + "”太小，已为您选择最佳清晰度 " + qualities.getString(1).replaceAll(" +", " "));
+				quality = 1;
+				videoDownloadUrl = getVideoDownload(video1, 0);
+			}
+		} else {
+			if (quality > qualities.size() - 1) {
+				System.out.println("输入的数字“" + quality + "”太大，已为您选择最差清晰度 " + qualities.getString(qualities.size() - 1).replaceAll(" +", " "));
+				quality = qualities.size() - 1;
+				videoDownloadUrl = getVideoDownload(video2, qualities2.size() - 1);
+			} else if (quality > 0)
+				videoDownloadUrl = getVideoDownload(video2, quality - 1);
+			else {
+				System.out.println("输入的数字“" + quality + "”太小，已为您选择最佳清晰度 " + qualities.getString(1).replaceAll(" +", " "));
+				quality = 1;
+				videoDownloadUrl = getVideoDownload(video2, 0);
+			}
+		}
+		System.out.println("\n请输入保存目录：");
+		Path path = Paths.get(scanner.next(), name.replaceAll("[/\\\\:*?<>|]", "_"));
+		System.out.println("\n下载选项：\n  1. 视频+音频（合并需要ffmpeg）\n  2. 仅视频\n  3. 仅音频\n请选择下载选项（输入 1~3 之间的整数）：");
+		int choice = scanner.nextInt();
+		if (choice > 3) {
+			System.out.println("输入的数字“" + choice + "”太大，已为您选择最后一个选项 仅音频");
+			choice = 3;
+		}
+		if (choice < 1) {
+			System.out.println("输入的数字“" + choice + "”太小，已为您选择第一个选项 视频+音频（合并需要ffmpeg）");
+			choice = 1;
+		}
+		switch (choice) {
+			case 1: {
+				System.out.println("\n请输入ffmpeg.exe的路径（跳过合并请填“#”）：");
+				String ffmpeg = scanner.next();
+				boolean videoSuccess, audioSuccess;
+				System.out.println();
+				if (videoDownloadUrl == null) {
+					System.out.print("\n无法获取 " + qualities.getString(quality).replaceAll(" +", " ") + " 的视频下载地址，已为您选择最佳清晰度 " + getQualityDescription(video2, video2.getJSONObject("dash").getJSONArray("video").getJSONObject(0).getIntValue("id")));
+					videoDownloadUrl = video2.getJSONObject("dash").getJSONArray("video").getJSONObject(0).getString("base_url");
+					System.out.println("，下载地址：" + videoDownloadUrl);
+				} else {
+					System.out.println("\n成功获取 " + qualities.getString(quality).replaceAll(" +", " ") + " 的视频下载地址：" + videoDownloadUrl);
+				}
+				String audioDownloadUrl = getAudioDownload(video2);
+				System.out.println("\n成功获取音频下载地址：" + audioDownloadUrl);
+				File video = ffmpeg.equals("#") ? new File(path + ".mp4") : new File(System.getProperty("user.dir"), "tmpVid.mp4");
+				File audio = ffmpeg.equals("#") ? new File(path + ".aac") : new File(System.getProperty("user.dir"), "tmpAud.aac");
+				System.out.println("\n正在下载视频至 " + video.getAbsolutePath());
+				long lenVid = download(videoDownloadUrl, video.getAbsolutePath());
+				videoSuccess = video.length() == lenVid;
+				System.out.println(videoSuccess ? "\n视频下载完毕" : "\n视频下载失败");
+				System.out.println("\n正在下载音频至 " + audio.getAbsolutePath());
+				long lenAud = download(audioDownloadUrl, audio.getAbsolutePath());
+				audioSuccess = audio.length() == lenAud;
+				System.out.println(audioSuccess ? "\n音频下载完毕" : "\n音频下载失败");
+				if (videoSuccess && audioSuccess && !ffmpeg.equals("#")) {
+					System.out.println("\n正在合并至 " + path + ".mp4");
+					File file = merge(ffmpeg.endsWith("ffmpeg.exe") ? new File(ffmpeg) : new File(ffmpeg, "ffmpeg.exe"), video, audio, new File(path + ".mp4"));
+					if (file != null) {
+						System.out.println("\n合并完毕");
+						video.deleteOnExit();
+						audio.deleteOnExit();
+					} else {
+						System.out.println("\n合并失败");
+					}
+				}
+				break;
+			}
+			case 2: {
+				boolean videoSuccess;
+				if (videoDownloadUrl == null) {
+					System.out.print("\n无法获取 " + qualities.getString(quality).replaceAll(" +", " ") + " 的视频下载地址，已为您选择最佳清晰度 " + getQualityDescription(video2, video2.getJSONObject("dash").getJSONArray("video").getJSONObject(0).getIntValue("id")));
+					videoDownloadUrl = video2.getJSONObject("dash").getJSONArray("video").getJSONObject(0).getString("base_url");
+					System.out.println("，下载地址：" + videoDownloadUrl);
+				} else {
+					System.out.println("\n成功获取 " + qualities.getString(quality).replaceAll(" +", " ") + " 的视频下载地址：" + videoDownloadUrl);
+				}
+				File video = new File(path + ".mp4");
+				System.out.println("\n正在下载至 " + video.getAbsolutePath());
+				long len = download(videoDownloadUrl, video.getAbsolutePath());
+				videoSuccess = video.length() == len;
+				System.out.println(videoSuccess ? "\n下载完毕" : "\n下载失败");
+				break;
+			}
+			case 3: {
+				boolean audioSuccess;
+				String audioDownloadUrl = getAudioDownload(video2);
+				System.out.println("\n成功获取音频下载地址：" + audioDownloadUrl);
+				File audio = new File(path + ".aac");
+				System.out.println("\n正在下载至 " + audio.getAbsolutePath());
+				long len = download(audioDownloadUrl, audio.getAbsolutePath());
+				audioSuccess = audio.length() == len;
+				System.out.println(audioSuccess ? "\n下载完毕" : "\n下载失败");
+				break;
+			}
+		}
+		System.out.println("\n程序运行结束，总耗时 " + getFormattedTime(System.currentTimeMillis() - beginTime, "mm:ss.SS"));
+	}
+
+	private static String getFormattedTime(int time, boolean hour) {
+		String result = "";
+		if (hour) {
+			result += time / 3600 + ":";
+		}
+		String min = (time % 3600) / 60 + "";
+		result += (min.length() < 2 ? "0" + min : min) + ":";
+		String sec = time %  60 + "";
+		result += sec.length() < 2 ? "0" + sec : sec;
+		return result;
+	}
+
+	private static String getFormattedTime(long time, String format) {
+		SimpleDateFormat ft = new SimpleDateFormat(format);
+		return ft.format(time);
+	}
+
+	private static File merge(File ffmpegExecutable, File video, File audio, File output) throws IOException, InterruptedException {
+		for (File f : Arrays.asList(ffmpegExecutable, video, audio)) {
+			if (!f.exists()) {
+				System.out.println("指定的文件“" + f.getAbsolutePath() + "”不存在");
+				return null;
+			}
+		}
+		if (output.exists())
+			output.delete();
+		ProcessBuilder pb = new ProcessBuilder(
+				ffmpegExecutable.getAbsolutePath(),
+				"-i", video.getAbsolutePath(),
+				"-i", audio.getAbsolutePath(),
+				"-vcodec", "copy",
+				"-acodec", "copy",
+				output.getAbsolutePath()
+		);
+		pb.redirectErrorStream(true);
+		Process process = pb.start();
+		process.waitFor();
+		if (!output.exists()) {
+			return null;
+		}
+		return output;
+	}
+
+	private static JSONArray summary(JSONArray qualities1, JSONArray qualities2, JSONObject video1) {
+		boolean watermark = true;
+		JSONArray watermarks = video1.getJSONArray("accept_watermark");
+		for (int i = 0; i < watermarks.size(); i++) {
+			if (!watermarks.getBoolean(i)) {
+				watermark = false;
+				break;
+			}
+		}
+		JSONArray qualities = new JSONArray();
+		if (!watermark) {
+			qualities.add(1);
+			for (int i = 0; i < qualities1.size(); i++)
+				if (!video1.getJSONArray("accept_watermark").getBoolean(i))
+					qualities.add(String.format("%-11s", qualities1.getString(i)) + watermark(video1.getJSONArray("accept_watermark").getBoolean(i)));
+				else
+					qualities.add(qualities1.getString(i));
+		} else {
+			qualities.add(0);
+		}
+		for (int i = 0; i < qualities2.size(); i++)
+			qualities.add(qualities2.getString(i));
+		return qualities;
+	}
+
+	private static String getQualityDescription(JSONObject video, int qNum) {
+		JSONArray descriptions = video.getJSONArray("accept_description");
+		JSONArray qualityNums = video.getJSONArray("accept_quality");
+		for (int i = 0; i < qualityNums.size(); i++) {
+			if (qualityNums.getIntValue(i) == qNum) {
+				return descriptions.getString(i);
+			}
+		}
+		return null;
+	}
+
+//	private static int getQualityNumber(JSONObject video, String qStr) {
+//		JSONArray descriptions = video.getJSONArray("accept_description");
+//		JSONArray qualityNums = video.getJSONArray("accept_quality");
+//		for (int i = 0; i < qualityNums.size(); i++) {
+//			if (qStr.startsWith(descriptions.getString(i))) {
+//				return qualityNums.getIntValue(i);
+//			}
+//		}
+//		return 0;
+//	}
+
+	private static String getVideoDownload(JSONObject video, int quality) {
+		int qualityNum = video.getJSONArray("accept_quality").getIntValue(quality);
+		JSONArray videos = video.getJSONObject("dash").getJSONArray("video");
+		for (int i = 0; i < videos.size(); i++) {
+			if (videos.getJSONObject(i).getIntValue("id") == qualityNum) {
+				return videos.getJSONObject(i).getString("base_url");
+			}
+		}
+		return null;
+	}
+
+	private static String getAudioDownload(JSONObject video) {
+		JSONArray audios = video.getJSONObject("dash").getJSONArray("audio");
+		return audios.getJSONObject(0).getString("base_url");
+	}
+
+	private static String watermark(boolean availability) {
+		if (availability) return "";
+		else return "无水印";
+	}
+
+	private static URLConnection readUrl(String url, String cookie) throws IOException {
+		String userAgent = UserAgentManager.getUserAgent();
+//		System.out.println("正在访问 " + url + "，使用 UA “" + userAgent + "”");
+		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 1081));
+		URLConnection request = (new URL(url)).openConnection(proxy);
+		request.setRequestProperty("User-Agent", userAgent);
+		if (!cookie.equals("#"))
+			request.setRequestProperty("Cookie", cookie);
+		System.setProperty("http.agent", userAgent);
+		request.connect();
+		return request;
+	}
+
+	private static JSONObject readJsonFromUrl(String url, String cookie) throws IOException {
+		return JSON.parseObject(IOUtils.toString((InputStream) readUrl(url, cookie).getContent(), StandardCharsets.UTF_8));
+	}
+
+	private static long download(String address, String path) {
+		int byteRead; // 1. 下载网络文件
+		URL url;
+		try {
+			url = new URL(address);
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+			return -1;
+		}
+		try {
+			URLConnection conn = url.openConnection(); // 2. 获取链接
+			conn.setRequestProperty("Referer", "https://www.bilibili.com");
+			InputStream inStream = conn.getInputStream(); // 3. 输入流
+			FileOutputStream fs = new FileOutputStream(path); // 4. 写入文件
+
+			byte[] buffer = new byte[1024];
+			String progress = "";
+			System.out.print("进度：");
+			while ((byteRead = inStream.read(buffer)) != -1) {
+				fs.write(buffer, 0, byteRead);
+				if (!progress.isEmpty())
+					for (int i = 0; i < progress.length(); i++)
+						System.out.print("\b");
+				progress = String.format("%.2f", (fs.getChannel().size() * 100.0 / conn.getContentLengthLong())) + "% (" + String.format("%,f", Math.round(fs.getChannel().size() / 1024.0) / 1024.0) + "MB / " + String.format("%,f", Math.round(conn.getContentLengthLong() / 1024.0) / 1024.0) + "MB)";
+				System.out.print(progress);
+			}
+			System.out.println();
+			inStream.close();
+			fs.close();
+			return conn.getContentLengthLong();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+}
---
+++ src/main/java/me/naptie/bilidownload/utils/UserAgentManager.java
@@ -0,0 +1,54 @@
+package me.naptie.bilidownload.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserAgentManager {
+
+    private static final List<String> userAgents = new ArrayList<String>() {{
+        add("Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5 Quark/4.2.1.138");
+        add("Mozilla/5.0 (Linux; Android 4.2.1; M040 Build/JOP40D) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.59 Mobile Safari/537.36");
+        add("Mozilla/5.0 (Linux; Android 10; ELS-AN00 Build/HUAWEIELS-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045230 Mobile Safari/537.36 V1_AND_SQ_8.4.1_1442_YYB_D QQ/8.4.1.4680 NetType/WIFI WebP/0.3.0 Pixel/1200 StatusBarHeight/142 SimpleUISwitch/0 QQTheme/1000");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F70 Safari/600.1.4");
+        add("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+        add("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36");
+        add("Mozilla/5.0 (Linux; Android 10; ELS-AN00 Build/HUAWEIELS-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2575 MMWEBSDK/200701 Mobile Safari/537.36 MMWEBID/4566 MicroMessenger/7.0.17.1720(0x27001135) Process/toolsmp WeChat/arm64 NetType/WIFI Language/zh_CN ABI/arm64");
+        add("Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko");
+        add("Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405");
+        add("Mozilla/5.0 (Windows; U; Windows NT 5.2;. en-US) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.108 Safari/537.36 Quark/4.2.1.138");
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763");
+        add("Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30");
+        add("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36");
+        add("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0");
+        add("Mozilla/5.0 (Linux; Android 10; ELS-AN00 Build/HUAWEIELS-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.108 Mobile Safari/537.36 baiduinput/8.2.8.151");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) CriOS/31.0.1650.18 Mobile/11B554a Safari/8536.25");
+        add("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15");
+        add("Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36");
+        add("Mozilla/5.0 (Linux; Android 10; ELS-AN00 Build/HUAWEIELS-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.108 Mobile Safari/537.36 CloudMusic/0.1.1 NeteaseMusic/7.2.10");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1");
+        add("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41");
+        add("Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)");
+        add("ZhihuHybrid DefaultBrowser com.zhihu.android/Futureve/6.49.0 Mozilla/5.0 (Linux; Android 10; ELS-AN00 Build/HUAWEIELS-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.108 Mobile Safari/537.36");
+        add("Mozilla/5.0 (Linux; U; Android 4.4.4; zh-cn; M351 Build/KTU84P) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30");
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.61");
+        add("Mozilla/5.0 (Linux; Android 10; ELS-AN00; HMSCore 5.0.0.304; GMSCore 20.12.14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 HuaweiBrowser/10.1.3.321 Mobile Safari/537.36");
+        add("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:65.0) Gecko/20100101 Firefox/65.0");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1");
+        add("Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0");
+        add("Mozilla/5.0 (Linux; U; Android 10; zh-Hans-CN; ELS-AN00 Build/HUAWEIELS-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.108 Quark/4.2.1.138 Mobile Safari/537.36");
+        add("Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/_BuildID_) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1");
+        add("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12");
+        add("Mozilla/5.0 (Linux; Android 10; ELS-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.116 Mobile Safari/537.36 EdgA/45.05.4.5036");
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36");
+        add("Mozilla/5.0 (Linux; Android 10; ELS-AN00 Build/HUAWEIELS-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045514 Mobile Safari/537.36 V1_AND_SQ_8.5.5_1630_YYB_D A_8050500 QQ/8.5.5.5105 NetType/WIFI WebP/0.3.0 Pixel/1200 StatusBarHeight/142 SimpleUISwitch/0 QQTheme/1000 InMagicWin/0");
+    }};
+
+    private static int i = (int) Math.floor(Math.random() * userAgents.size());
+
+    public static String getUserAgent() {
+        if (i+1 < userAgents.size()) i++;
+        else i = 0;
+        return userAgents.get(i);
+    }
+}
